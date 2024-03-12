7f34d59aa7cfbed654bca601e3300767cd057b82
AdisRain19 <adisrain4@gmail.com>
2024-03-12 20:13:20 +0700

    menambahkan folder week3

---
+++ regist.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrasi</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style>
+        body {
+            display: flex;
+            background-image: url(https://images.pexels.com/photos/8657665/pexels-photo-8657665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2);
+            background-size: cover;
+            font-family: Arial, sans-serif;
+        }
+        .form_container {
+            color: #fff;
+            max-width: 500px;
+            margin: 50px auto;
+            padding: 30px;
+            background-color: transparent;
+            backdrop-filter: blur(20px);
+            border-radius: 30px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.443);
+        }
+        .form_container h2 {
+            text-align: center;
+            margin-bottom: 30px;
+            font-weight: bold;
+        }
+        .form_container input {
+            border-radius: 40px;
+            border: none;
+            outline: none;
+        }
+        .form_container select {
+            border-radius: 40px;
+            border: none;
+            outline: none;
+        }
+        .form_container button{
+            width: 100%;
+            border-radius: 40px;
+            border: none;
+            outline: none;
+        }
+        .form-control:focus {
+            box-shadow: none;
+        }
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+        
+    </style>
+
+<body>
+  <div class="form_container">
+    <h2>Form Registrasi Asdos</h2>
+    <div class="container">
+      <form method="POST" action="proses.php">
+        <div class="form-group row">
+          <label for="nim" class="col-4 col-form-label">NIM</label>
+          <div class="col-8">
+            <input id="nim" name="nim" placeholder="Masukkan NIM anda" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="nama" class="col-4 col-form-label">Nama</label>
+          <div class="col-8">
+            <input id="nama" name="nama" placeholder="Masukkan Nama Lengkap" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-4">Jenis Kelamin</label>
+          <div class="col-8">
+            <div class="custom-control custom-radio custom-control-inline">
+              <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="P">
+              <label for="jk_0" class="custom-control-label">Perempuan</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline">
+              <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="L">
+              <label for="jk_1" class="custom-control-label">Laki-Laki</label>
+            </div>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="prodi" class="col-4 col-form-label">Prodi</label>
+          <div class="col-8">
+            <select id="prodi" name="prodi" class="custom-select">
+              <option value="TI">Teknik Informatika</option>
+              <option value="SI">Sistem Informasi</option>
+              <option value="BD">Bisnis Digital</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-4">Skill</label>
+          <div class="col-8">
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_0" type="checkbox" class="custom-control-input" value="html">
+              <label for="skill[]_0" class="custom-control-label">HTML</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_1" type="checkbox" class="custom-control-input" value="css">
+              <label for="skill[]_1" class="custom-control-label">CSS</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_2" type="checkbox" class="custom-control-input" value="js">
+              <label for="skill[]_2" class="custom-control-label">JAVASCRIPT</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_3" type="checkbox" class="custom-control-input" value="rwd">
+              <label for="skill[]_3" class="custom-control-label">RWD</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_4" type="checkbox" class="custom-control-input" value="php">
+              <label for="skill[]_4" class="custom-control-label">PHP</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_5" type="checkbox" class="custom-control-input" value="java">
+              <label for="skill[]_5" class="custom-control-label">JAVA</label>
+            </div>
+            <div class="custom-control custom-checkbox custom-control-inline">
+              <input name="skill[]" id="skill[]_6" type="checkbox" class="custom-control-input" value="phyton">
+              <label for="skill[]_6" class="custom-control-label">PHYTON</label>
+            </div>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="domisili" class="col-4 col-form-label">Domisili</label>
+          <div class="col-8">
+            <select id="domisili" name="domisili" class="custom-select">
+              <option value="jakarta">Jakarta</option>
+              <option value="depok">Depok</option>
+              <option value="bogor">Bogor</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="email" class="col-4 col-form-label">Email</label>
+          <div class="col-8">
+            <input id="email" name="email" type="text" class="form-control">
+          </div>
+        </div>
+        <div class="form-group row">
+          <div class="offset-4 col-8">
+            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+</body>
+
+</html>
